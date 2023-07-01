1d8bcd3ea2e911576dd1b162c1b8ad3495dbd2c5
Katarzyna Śliwka <sliwkakatarzyna12@gmail.com>
2023-07-01 16:29:18 +0200

    creating todoitem component

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoItem: React.FC<{ text: string }> = (props) => {
+  return (
+    <>
+      <li>{props.text}</li>
+    </>
+  );
+};
+
+export default TodoItem;
+
+//props are always an object
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,17 @@
-function Todos() {
+import React from "react";
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn Typescript</li>
+      {props.items.map((item) => (
+        <TodoItem text={item.text}  key={item.id}/>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
+
+// FC - FUNCTIONAL COMPONENT - CAN USE PROPS
